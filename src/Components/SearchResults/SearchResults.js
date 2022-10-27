bd2508d45faa7fbd570c4950920a8ae2c2718218
Lucas Burney <burnelucasd@gmail.com>
2022-10-27 05:05:53 -0500

    Add changes until step 34ish

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './SearchResults.css';
+
+import TrackList from '../TextTrackList/TextTrackList.js';
+
+class SearchResults extends React.Component {
+
+  render() {  
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList tracks={this.props.searchResults}/>
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    )
+  }
+}
+
+export default SearchResults;
